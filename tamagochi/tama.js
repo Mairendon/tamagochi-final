cccc54a3ffea819183bdde500a229085c89e5dbf
AndreaGranada <andreaalejandre93@gmail.com>
2022-12-18 02:22:50 +0100

    fondo en movimiento y suciedad

--- tamagochi/tama.js
+++ tamagochi/tama.js
@@ -1,8 +1,9 @@
-export function Tama(nombre, felicidad, energia, diversion){
+export function Tama(nombre, felicidad, energia, suciedad, diversion){
     this.nombre = nombre;
     this.felicidad = felicidad;
     this.energia = energia;
-    //this.suciedad = suciedad;
+    //ACTIVO SUCIEDAD
+    this.suciedad = suciedad;
     this.diversion = diversion;
     //this.salud = salud;
     //this.amor = amor;
@@ -15,22 +16,25 @@ export function Tama(nombre, felicidad, energia, diversion){
 }
 Tama.prototype.vida = function () {
   
-    var total = this.felicidad + this.energia + this.diversion;
+    var total = this.felicidad + this.energia + this.diversion + this.suciedad;
 
-    if( this.felicidad <= 0 || this.energia <= 0 || this.diversion <= 0){
+    if( this.felicidad <= 0 || this.energia <= 0 || this.diversion <= 0 || this.suciedad <= 0){
         this.machango.src = "./images/muerto.jpeg"; /*imagen jaspi muerto */
         this.machango.innerText = "he muerto"
     } else if(total <= 150){ 
         this.machango.src = " "; /*imagen jaspi pachucho */
+
     } else if(total > 150) {
         this.machango.src = "./images/vivo.jpeg "; /*imagen jaspi :) */
     }
 this.felicidad = this.felicidad - this.time;
 this.energia = this.energia - this.time; 
 this.diversion = this.diversion - this.time; 
+this.suciedad = this.suciedad - this.time;
 document.getElementById('felicidad').innerText = this.felicidad + '%';
 document.getElementById('energia').innerText = this.energia + '%';
-document.getElementById('diversion').innerText = this.diversion + '%';   
+document.getElementById('diversion').innerText = this.diversion + '%';
+document.getElementById('suciedad').innerText = this.suciedad + '%';      
 }
 
 Tama.prototype.comer = function (alimento) {
@@ -38,11 +42,13 @@ Tama.prototype.comer = function (alimento) {
    
     if(this.energia >= 100){
         this.felicidad--;
+        this.suciedad--;
         return 'Estoy llenito'
     } else if( this.energia > 0){
         this.machango.src = "./images/cama.png"
         this.felicidad++;
         this.energia++;
+        this.suciedad--;
         return '¡DAAME DE COMER!'
     } else {
         return 'Los muertos no hablan'
@@ -75,17 +81,46 @@ Tama.prototype.toy = function () {
     //this.amor++;
     if (this.diversion <= 25){
         this.diversion++
+        this.suciedad--;
         return 'Me matas de aburrimiento'
     } else if (this.diversion > 40){
         this.felicidad++;
         this.energia--;
         this.diversion++;
+        this.suciedad--;
         return '¡Quiero jugar!'
     } else {
         this.felicidad++;
         this.energia--;
         this.diversion++;
+        this.suciedad--;
         return '¡No me canso de jugar!'
+    }
+}
+////---------Prueba DUCHA---------------
+Tama.prototype.lavar = function () {
+    this.felicidad++;
+    this.energia--;
+    //this.suciedad - 4
+    this.diversion--;
+    this.suciedad++;
+    //this.amor++;
+    if (this.suciedad <= 30){
+        this.diversion++;
+        this.suciedad++; // si pongo +20 no suma vente punntos de golpe
+        return 'Ya era hora... Olia a zorruno'
+    } else if (this.suciedad <= 60){
+        this.felicidad++;
+        this.energia--;
+        this.diversion--;
+        this.suciedad++;
+        return 'No me quiero lavar no me voy a duchar asi cochino me quiero quedar'
+    } else {
+        this.felicidad--;
+        this.energia--;
+        this.diversion--;
+        this.suciedad++;
+        return '¡Que ya estoy limpio!'
     }
 }
 
